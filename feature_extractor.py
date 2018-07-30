067b659f7e1c4c8a55cc4d57a935b01529c5854c
Kateryna Peikova <peikovakate@gmail.com>
2018-07-30 17:15:57 +0300

    citcles might work

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,8 +1,10 @@
 import numpy as np
 import pandas as pd
 from math import log2
+from geopy import distance
 
 class FeatureExtractor:
+
     lonMin = -74.05  # minimum longitude
     lonMax = -73.8
     lonStep = 0.01  # defines cell size
@@ -10,6 +12,11 @@ class FeatureExtractor:
     latMin = 40.65  # minimum latitude
     latMax = 40.85
     latStep = 0.0025  # defines cell size
+
+    categories = []
+    business_name = ''
+    is_cells = True
+    radius = 200
 
     def __init__(self, venues, transitions):
         self.venues = {}
@@ -20,14 +27,17 @@ class FeatureExtractor:
         self.transitions = transitions.values
 
     def _get_coords(self, venue_id):
-        # venue = self.venues[self.venues.venue_id == venue_id]
-        # return venue.latitude.values[0], venue.longitude.values[0]
         return self.venues[venue_id]['latitude'], self.venues[venue_id]['longitude']
 
     def calculate_transitions(self):
-        latLen = int((self.latMax - self.latMin) / self.latStep) + 1  # number of cells on the y-axis
-        lonLen = int((self.lonMax - self.lonMin) / self.lonStep) + 1  # number of cells on the x-axis
-        self.transitions_grid = np.zeros((latLen, lonLen, 4))
+        print('calc transitions')
+        if self.is_cells:
+            latLen = int((self.latMax - self.latMin) / self.latStep) + 1  # number of cells on the y-axis
+            lonLen = int((self.lonMax - self.lonMin) / self.lonStep) + 1  # number of cells on the x-axis
+            self.transitions_grid = np.zeros((latLen, lonLen, 4))
+        else:
+            business_n = len(self.business_ids)
+            self.transitions_grid = np.zeros((business_n, 4))
 
         # [0] - area popularity
         # [1] - transition density
@@ -36,26 +46,33 @@ class FeatureExtractor:
         for i in range(0, len(self.transitions)):
             a_id = self.transitions[i][0]  # id of A place
             b_id = self.transitions[i][1]  # id of B place
-            a_lat, a_long = self._get_coords(a_id)  # coordinates of A place
-            b_lat, b_long = self._get_coords(b_id)  # coordinates of B place
-            a_cy, a_cx = self._get_cell_indexes((a_lat, a_long))
-            b_cy, b_cx = self._get_cell_indexes((b_lat, b_long))
-            if(i%10000==0):
+            if self.is_cells:
+                a_lat, a_long = self._get_coords(a_id)  # coordinates of A place
+                b_lat, b_long = self._get_coords(b_id)  # coordinates of B place
+                a_index = self._get_area_index((a_lat, a_long))
+                b_index = self._get_area_index((b_lat, b_long))
+            else:
+                a_index = self._get_circle_index(a_id)
+                b_index = self._get_circle_index(b_id)
+            if(i%10000 == 0):
                 print(i)
-            if a_cy != None and b_cy != None:
+            if a_index != None and b_index != None:
                 #  area popularity
-                self.transitions_grid[a_cy, a_cx][0] += 1
-                self.transitions_grid[b_cy, b_cx][0] += 1
+                self.transitions_grid[a_index][0] += 1
+                self.transitions_grid[b_index][0] += 1
                 #  transition density
-                if a_cx == b_cx and a_cy == b_cy:
-                    self.transitions_grid[a_cy, a_cx][1] += 1
+                if a_index == b_index:
+                    self.transitions_grid[a_index][1] += 1
                 #  incoming flow
                 else:
-                    self.transitions_grid[b_cy, b_cx][2] += 1
+                    self.transitions_grid[b_index][2] += 1
                 #  transition quality
 
+    def _get_ids_of_business(self):
+        ids = [venue['venue_id'] for venue in self.venues.values() if venue['title'] == self.business_name]
+        return ids
 
-    def calculate_squares(self):
+    def _calc_rects(self):
         latLen = int((self.latMax - self.latMin) / self.latStep) + 1  # number of cells on the y-axis
         lonLen = int((self.lonMax - self.lonMin) / self.lonStep) + 1  # number of cells on the x-axis
 
@@ -65,61 +82,114 @@ class FeatureExtractor:
                 self.venues_grid[y][x] = np.array([])
 
         for key in self.venues:
-            cy, cx = self._get_cell_indexes((self.venues[key]['latitude'], self.venues[key]['longitude']))
-            if cy != None:
-                self.venues_grid[cy, cx] = np.append(self.venues_grid[cy, cx], key)
+            index = self._get_area_index((self.venues[key]['latitude'], self.venues[key]['longitude']))
+            if index != None:
+                self.venues_grid[index] = np.append(self.venues_grid[index], key)
 
-    def _get_cell_indexes(self, coord):
+    def _calc_circles(self):
+        print('calc circles')
+        self.business_ids = self._get_ids_of_business()
+        self.business_venues = [self.venues[key] for key in self.business_ids]
+        n_business = len(self.business_ids)
+        self.venues_grid = np.zeros(n_business, dtype=np.ndarray)
+        for index in range(n_business):
+
+            b_id = self.business_ids[index]
+
+            business_coords = (self.venues[b_id]['latitude'], self.venues[b_id]['longitude'])
+            self.venues_grid[index] = self._get_places_around(business_coords)
+            print(index, b_id, len(self.venues_grid[index]))
+
+    def calculate_areas(self):
+        print('calc areas')
+        if self.is_cells:
+            self._calc_rects()
+        else:
+            self._calc_circles()
+
+    def _get_area_index(self, coord):
         lat = coord[0]
         longit = coord[1]
-        # if outside the grid then ingore point
-        if (lat < self.latMin) or (lat > self.latMax) or (longit < self.lonMin) or (longit > self.lonMax):
-            return None, None
-        cx = int((longit - self.lonMin) / self.lonStep)
-        cy = int((lat - self.latMin) / self.latStep)
-        return cy, cx
+        if self.is_cells:
+            # if outside the grid then ingore point
+            if (lat < self.latMin) or (lat > self.latMax) or (longit < self.lonMin) or (longit > self.lonMax):
+                return None
+            cx = int((longit - self.lonMin) / self.lonStep)
+            cy = int((lat - self.latMin) / self.latStep)
+            return (cy, cx)
+        # DO NOT use this function for circle areas
+        else:
+            print('_get_area_index used for circle')
+            return 0
 
-    def _get_cell_coordinates(self, cy, cx):
-        # top left corner
-        lat = self.latMin+self.latStep*cy
-        long = self.lonMin+self.lonStep*cx
+    # what if venue in more then one circle???
+    def _get_circle_index(self, venue_id):
+        for circle_index in range(len(self.venues_grid)):
+            if venue_id in self.venues_grid[circle_index]:
+                return circle_index
+        return None
+
+    def _get_cell_coordinates(self, index):
+        if self.is_cells:
+            cy = index[0]
+            cx = index[1]
+            # top left corner
+            lat = self.latMin+self.latStep*cy
+            long = self.lonMin+self.lonStep*cx
+        else:
+            venue = self.venues[self.business_ids[index]]
+            lat = venue['latitude']
+            long = venue['longitude']
         return lat, long
 
-    def _get_density(self, cy, cx):
-        return len(self.venues_grid[cy][cx])
+    def _get_density(self, index):
+        return len(self.venues_grid[index])
 
-    def _get_number_of_category(self, cy, cx, categories):
-        cell = self.venues_grid[cy][cx]
+    def _get_number_of_category(self, index, categories):
+        cell = self.venues_grid[index]
         return len([i for i in cell if self.venues[i]['category'] in categories])
         # return len([i for i in cell if self.venues.category[self.venues.venue_id == i].values[0] in categories])
 
     # not needed???
-    def _get_number_categories(self, cy, cx):
-        cell = self.venues_grid[cy][cx]
+    def _get_number_categories(self, index):
+        cell = self.venues_grid[index]
         categories = [self.venues[self.venues.venue_id == i]['category'].values[0] for i in cell]
         # returning number of unique categories
         return len(set(categories))
 
-    def _get_neighb_entropy(self, cy, cx):
+    def _get_neighb_entropy(self, index):
         entropy = 0
-        density = self._get_density(cy, cx)
+        density = self._get_density(index)
         for cat in self.categories:
-            n_c = self._get_number_of_category(cy, cx, cat)
+            n_c = self._get_number_of_category(index, cat)
             # log(0) = undefined
             if n_c != 0 and density!=0:
                 entropy -= n_c / density * log2(n_c/density)
         return entropy
 
-    def _get_competitiveness(self, cy, cx, category):
-        density = self._get_density(cy, cx)
+    def _get_competitiveness(self, index, category):
+        density = self._get_density(index)
         if density == 0:
             return 0
-        return - self._get_number_of_category(cy,cx, category)/self._get_density(cy, cx)
+        return - self._get_number_of_category(index, category)/self._get_density(index)
 
-    def calculate_features(self, categories, business_name):
+    def _calc_features_for_circles(self):
+        for index in range(len(self.venues_grid)):
+            print(index)
+            self.features['venue_id'] = np.append(self.features['venue_id'], self.business_ids[index])
+            self._calculate_features_for_area(index)
+
+    def _calc_features_for_rectangles(self):
+        for cy in range(len(self.venues_grid)):
+            for cx in range(len(self.venues_grid[cy])):
+                print(cy, cx)
+                self.features['cy'] = np.append(self.features['cy'], cy)
+                self.features['cx'] = np.append(self.features['cx'], cx)
+                self._calculate_features_for_area((cy, cx))
+
+    def calculate_features(self):
+        print('calc features')
         self.features = {
-            'cy': np.array([]),
-            'cx': np.array([]),
             'latitude': np.array([]),
             'longitude': np.array([]),
             'density': np.array([]),
@@ -130,59 +200,63 @@ class FeatureExtractor:
             'incoming_flow': np.array([]),
             'average_check_ins': np.array([]),
         }
-        #check if vanues_grid calculated
-        if self.venues_grid != None:
-            for cy in range(len(self.venues_grid)):
-                for cx in range(len(self.venues_grid[cy])):
-                    print(cy, cx)
-                    self._calculate_features_for_cell(cy, cx, categories, business_name)
-
-
+        if self.is_cells:
+            self.features['cy'] = np.array([])
+            self.features['cx'] = np.array([])
+            self._calc_features_for_rectangles()
+        else:
+            self.features['venue_id'] = np.array([])
+            self._calc_features_for_circles()
 
         self.features_dataframe = pd.DataFrame(data=self.features)
-        # check if transitions processed
-        # if self.transitions_grid != None:
-        #     pass
 
-    def _calculate_features_for_cell(self, cy, cx, categories, business_name):
-        self.features['cy'] = np.append(self.features['cy'], cy)
-        self.features['cx'] = np.append(self.features['cx'], cx)
-        lat, lon = self._get_cell_coordinates(cy, cx)
+    def _calculate_features_for_area(self, index):
+        lat, lon = self._get_cell_coordinates(index)
         self.features['latitude'] = np.append(self.features['latitude'], lat)
         self.features['longitude'] = np.append(self.features['longitude'], lon)
         # GEOGRAPHIC FEATURES
         # density
-        den = self._get_density(cy, cx)
+        den = self._get_density(index)
         self.features['density'] = np.append(self.features['density'], den)
         # neighbor entropy
-        neight_ent = self._get_neighb_entropy(cy, cx)
+        neight_ent = self._get_neighb_entropy(index)
         self.features['neighbors_entropy'] = np.append(self.features['neighbors_entropy'], neight_ent)
         # competitiveness
-        comp = self._get_competitiveness(cy, cx, categories)
+        comp = self._get_competitiveness(index, self.categories)
         self.features['competitiveness'] = np.append(self.features['competitiveness'], comp)
 
         # MOBILITY FEATURES
         # area popularity
-        self.features['area_popularity'] = np.append(self.features['area_popularity'], self.transitions_grid[cy][cx][0])
+        self.features['area_popularity'] = np.append(self.features['area_popularity'], self.transitions_grid[index][0])
         # transition density
-        self.features['transition_density'] = np.append(self.features['transition_density'], self.transitions_grid[cy][cx][1])
+        self.features['transition_density'] = np.append(self.features['transition_density'], self.transitions_grid[index][1])
         # incoming flow
-        self.features['incoming_flow'] = np.append(self.features['incoming_flow'], self.transitions_grid[cy][cx][2])
+        self.features['incoming_flow'] = np.append(self.features['incoming_flow'], self.transitions_grid[index][2])
 
         # aver check ins for given business
-        aver_check_in = self._get_aver_check_ins_for_business(cy, cx, business_name)
+        aver_check_in = self._get_aver_check_ins_for_business(index, self.business_name)
         self.features['average_check_ins'] = np.append(self.features['average_check_ins'], aver_check_in)
 
 
     def save_into_file(self, filename):
         self.features_dataframe.to_csv(filename)
 
-    def _get_aver_check_ins_for_business(self, cy, cx, name):
-        check_ins = [self.venues[id]['total_check-ins'] for id in self.venues_grid[cy][cx] if self.venues[id]['title'] == name]
+    def _get_aver_check_ins_for_business(self, index, name):
+        check_ins = [self.venues[id]['total_check-ins'] for id in self.venues_grid[index] if self.venues[id]['title'] == name]
         if len(check_ins) != 0:
             return sum(check_ins)/len(check_ins)
         else:
             return 0
+
+
+    def _get_places_around(self, center_coordinates):
+        # circle area
+        places_ids = []
+        for key in self.venues:
+            dist = distance.distance(center_coordinates, (self.venues[key]['latitude'], self.venues[key]['longitude'])).m
+            if dist <= self.radius:
+                places_ids.append(self.venues[key]['venue_id'])
+        return places_ids
 
 
 
--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import pandas as pd
 from feature_extractor import FeatureExtractor
+# Reading files
 transitions = pd.read_csv('shared_data/new york_placenet_transitions.txt', sep=',', names=['A_id', 'B_id', 'A_datetime', 'B_datetime'])
 ny_venues = pd.read_csv('shared_data/ny_venues.csv', sep=',')
+
 extractor = FeatureExtractor(ny_venues, transitions)
 
-extractor.calculate_squares()
+# Define model target
+# extractor.business_name = 'Subway'
+# extractor.categories = ['Sandwiches', 'Fast Food']
+
+extractor.business_name = 'Blockbuster'
+extractor.categories = ["Video Store"]
+
+
+extractor.is_cells = False
+
+
+
+extractor.calculate_areas()
 extractor.calculate_transitions()
 
-extractor.calculate_features(['Sandwiches', 'Fast Food'], 'Subway')
+extractor.calculate_features()
+
+# Saving features data into file
 extractor.save_into_file('features.csv')
