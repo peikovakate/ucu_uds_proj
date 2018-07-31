0acfb36d0221e0de9c86abffcb5712f61d1a05ab
Kateryna Peikova <peikovakate@gmail.com>
2018-07-31 12:45:44 +0300

    changed target feature, added transition quality feature

--- feature_extractor.py
+++ feature_extractor.py
@@ -19,7 +19,10 @@ class FeatureExtractor:
     radius = 200
 
     def __init__(self, venues, transitions):
+        self.features_dataframe = pd.DataFrame()
+        self.features = {}
         self.venues = {}
+
         for index, row in venues.iterrows():
             self.venues[row.venue_id] = row.to_dict()
         # self.venues = venues
@@ -28,6 +31,13 @@ class FeatureExtractor:
 
     def _get_coords(self, venue_id):
         return self.venues[venue_id]['latitude'], self.venues[venue_id]['longitude']
+
+    def calculate_areas(self):
+        print('calc areas')
+        if self.is_cells:
+            self._calc_rects()
+        else:
+            self._calc_circles()
 
     def calculate_transitions(self):
         print('calc transitions')
@@ -66,7 +76,7 @@ class FeatureExtractor:
                 #  incoming flow
                 else:
                     self.transitions_grid[b_index][2] += 1
-                #  transition quality
+
 
     def _get_ids_of_business(self):
         ids = [venue['venue_id'] for venue in self.venues.values() if venue['title'] == self.business_name]
@@ -89,7 +99,6 @@ class FeatureExtractor:
     def _calc_circles(self):
         print('calc circles')
         self.business_ids = self._get_ids_of_business()
-        self.business_venues = [self.venues[key] for key in self.business_ids]
         n_business = len(self.business_ids)
         self.venues_grid = np.zeros(n_business, dtype=np.ndarray)
         for index in range(n_business):
@@ -99,13 +108,6 @@ class FeatureExtractor:
             business_coords = (self.venues[b_id]['latitude'], self.venues[b_id]['longitude'])
             self.venues_grid[index] = self._get_places_around(business_coords)
             print(index, b_id, len(self.venues_grid[index]))
-
-    def calculate_areas(self):
-        print('calc areas')
-        if self.is_cells:
-            self._calc_rects()
-        else:
-            self._calc_circles()
 
     def _get_area_index(self, coord):
         lat = coord[0]
@@ -150,20 +152,13 @@ class FeatureExtractor:
         return len([i for i in cell if self.venues[i]['category'] in categories])
         # return len([i for i in cell if self.venues.category[self.venues.venue_id == i].values[0] in categories])
 
-    # not needed???
-    def _get_number_categories(self, index):
-        cell = self.venues_grid[index]
-        categories = [self.venues[self.venues.venue_id == i]['category'].values[0] for i in cell]
-        # returning number of unique categories
-        return len(set(categories))
-
     def _get_neighb_entropy(self, index):
         entropy = 0
         density = self._get_density(index)
         for cat in self.all_categories:
             n_c = self._get_number_of_category(index, cat)
             # log(0) = undefined
-            if n_c != 0 and density!=0:
+            if n_c != 0 and density != 0:
                 entropy -= n_c / density * log2(n_c/density)
         return entropy
 
@@ -176,7 +171,12 @@ class FeatureExtractor:
     def _calc_features_for_circles(self):
         for index in range(len(self.venues_grid)):
             print(index)
-            self.features['venue_id'] = np.append(self.features['venue_id'], self.business_ids[index])
+            venue_id = self.business_ids[index]
+            self.features['venue_id'] = np.append(self.features['venue_id'], venue_id)
+
+            target_check_in = self.venues[venue_id]['total_check-ins']
+            self.features['target_check_ins'] = np.append(self.features['target_check_ins'], target_check_in)
+
             self._calculate_features_for_area(index)
 
     def _calc_features_for_rectangles(self):
@@ -185,27 +185,39 @@ class FeatureExtractor:
                 print(cy, cx)
                 self.features['cy'] = np.append(self.features['cy'], cy)
                 self.features['cx'] = np.append(self.features['cx'], cx)
-                self._calculate_features_for_area((cy, cx))
+                index = (cy, cx)
+
+                # aver check ins for given business
+                aver_check_in = self._get_aver_check_ins_for_business(index, self.business_name)
+                self.features['average_check_ins'] = np.append(self.features['average_check_ins'], aver_check_in)
+
+                # features that don't depend on form of area
+                self._calculate_features_for_area(index)
 
     def calculate_features(self):
         print('calc features')
-        self.features = {
-            'latitude': np.array([]),
-            'longitude': np.array([]),
-            'density': np.array([]),
-            'neighbors_entropy': np.array([]),
-            'competitiveness': np.array([]),
-            'area_popularity': np.array([]),
-            'transition_density': np.array([]),
-            'incoming_flow': np.array([]),
-            'average_check_ins': np.array([]),
-        }
         if self.is_cells:
             self.features['cy'] = np.array([])
             self.features['cx'] = np.array([])
-            self._calc_features_for_rectangles()
         else:
             self.features['venue_id'] = np.array([])
+
+        self.features['latitude'] = np.array([])
+        self.features['longitude'] = np.array([])
+        self.features['density'] = np.array([])
+        self.features['neighbors_entropy'] = np.array([])
+        self.features['competitiveness'] = np.array([])
+        self.features['area_popularity'] = np.array([])
+        self.features['transition_density'] = np.array([])
+        self.features['incoming_flow'] = np.array([])
+        self.features['transition quality'] = np.array([])
+        self.features['n_same_business_in_area'] = np.array([])
+
+        if self.is_cells:
+            self.features['average_check_ins'] = np.array([])
+            self._calc_features_for_rectangles()
+        else:
+            self.features['target_check_ins'] = np.array([])
             self._calc_features_for_circles()
 
         self.features_dataframe = pd.DataFrame(data=self.features)
@@ -232,22 +244,31 @@ class FeatureExtractor:
         self.features['transition_density'] = np.append(self.features['transition_density'], self.transitions_grid[index][1])
         # incoming flow
         self.features['incoming_flow'] = np.append(self.features['incoming_flow'], self.transitions_grid[index][2])
+        # transition quality
+        trans_quality = self._get_transition_quality(index)
+        self.features['transition quality'] = np.append(self.features['transition quality'], trans_quality)
 
-        # aver check ins for given business
-        aver_check_in = self._get_aver_check_ins_for_business(index, self.business_name)
-        self.features['average_check_ins'] = np.append(self.features['average_check_ins'], aver_check_in)
+        # CUSTOM FEATURES
+        n_business = self._get_number_of_business_in_same_area(index, self.business_name)
+        self.features['n_same_business_in_area'] = np.append(self.features['n_same_business_in_area'], n_business)
 
 
     def save_into_file(self, filename):
         self.features_dataframe.to_csv(filename)
 
     def _get_aver_check_ins_for_business(self, index, name):
-        check_ins = [self.venues[id]['total_check-ins'] for id in self.venues_grid[index] if self.venues[id]['title'] == name]
+        check_ins = [self.venues[venue_id]['total_check-ins'] for venue_id in self.venues_grid[index] if self.venues[venue_id]['title'] == name]
         if len(check_ins) != 0:
             return sum(check_ins)/len(check_ins)
         else:
             return 0
 
+    def _get_number_of_business_in_same_area(self, index, name):
+        n = len([self.venues[venue_id] for venue_id in self.venues_grid[index] if self.venues[venue_id] == name])
+        # doesn't count the center business if circles
+        if not self.is_cells:
+            n -= 1
+        return n
 
     def _get_places_around(self, center_coordinates):
         # circle area
@@ -258,9 +279,26 @@ class FeatureExtractor:
                 places_ids.append(self.venues[key]['venue_id'])
         return places_ids
 
+    def _get_transition_quality(self, area_index):
+        # IS NOT implemented for rectangles!
+        if self.is_cells:
+            return 0
+        l_id = self.business_ids[area_index]
+        l_category = self.venues[l_id]['category']
+        quality = 0
+        for p_id in self.venues_grid[area_index]:
+            cp = self.venues[p_id]['total_check-in']
+            quality += self._prob_of_trans(p_id, l_category, cp)*cp
+        return quality
 
-
-
+    def _prob_of_trans(self, p_id, categoty_l, cp):
+        count = 0
+        for i in range(0, len(self.transitions)):
+            a_id = self.transitions[i][0]
+            b_id = self.transitions[i][1]
+            if p_id == a_id and self.venues[b_id]['category'] == categoty_l:
+                count += 1
+        return count/cp
 
 
 
